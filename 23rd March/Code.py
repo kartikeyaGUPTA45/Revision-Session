62b865d210d3c48fb050ef99a556839bc1b7b18c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-03-23 22:05:26 +0530

    Create Code.py

---
+++ 23rd March/Code.py
@@ -0,0 +1,42 @@
+a = 12
+b = 4
+
+sum = a+b
+
+if  sum > 50:
+    print("Sum is greater than 50")
+else :
+    print("Sum is less than or equal to 50")
+
+
+a = 45
+
+if a%2 == 0:
+    print("Number is even")
+else:
+    print("Number is Odd")
+
+a = 37
+rem = a%10
+
+print(rem)
+
+if rem == 0:
+    print("Rem is zero")
+elif rem%2 == 0:
+    print("Rem is a multiple of 2")
+else :
+    print("Rem is not a multiple of 2")
+
+
+a = 340
+b = 1200
+c = 56
+
+if a>b and a>c:
+    print("A is the maximum")
+elif b>c:
+    print("B is the maximum")
+else:
+    print("C is the maximum")
+
