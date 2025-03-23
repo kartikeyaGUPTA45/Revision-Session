469ac73edf71afddadcdc58204026ba423f76698
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-03-23 22:55:33 +0530

    Update Code.py

--- 23rd March/Code.py
+++ 23rd March/Code.py
@@ -9,12 +9,8 @@ else :
     print("Sum is less than or equal to 50")
 
 
-a = 45
-
-if a%2 == 0:
-    print("Number is even")
-else:
-    print("Number is Odd")
+a = 44
+print("Number is Even") if a%2 == 0 else print("Number is Odd")
 
 a = 37
 rem = a%10
@@ -39,4 +35,68 @@ elif b>c:
     print("B is the maximum")
 else:
     print("C is the maximum")
+
+
+a = 59
+if a%2 == 0:
+    print("Number is even")
+    if a>100:
+        print("Number is greater than 100")
+    elif a>50:
+        print("Number is greater than 50 but less than 100")
+    else :
+        print("Number is less than 50")
+else :
+    print("Number is Odd")
+    if (a>50):
+        print("Number is greater than 50")
+    else:
+        print("Number is less than 50")
+
+
+i = 1
+while (i<=10):
+    print("******")
+    i+=1
+
+i = 10
+while (i>=1):
+    print("******")
+    i-=1
+
+
+n = 20 
+i = 1
+sum = 0
+while (i<=n):
+    sum+=i;
+    i+=1
+
+print(sum)
+
+a = 23
+b = 67
+
+while (a<=b):
+    if a%2 == 0:
+        print(a)
+    a+=1    
+
+
+n = 5
+i = 1
+
+while (i<=10):
+    print(n*i)
+    i+=1
+
+
+n = 24
+i=1
+
+while(i<=n):
+    if n%i==0:
+        print(i)
+    i+=1    
+
 
